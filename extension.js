597abeda0ab2bbe8c498c8a63a614f15c4603dba
Berk Mollamustafaoglu <berkmolla@gmail.com>
2017-10-06 14:56:49 +0100

    Fetch bounty amount from server and display it on the page.

--- extension.js
+++ extension.js
@@ -1,16 +1,56 @@
 
 window.addEventListener('DOMContentLoaded', event => {
+
+    injectBountyInfoIntoPage();
+
+    const url = window.location.href;
+    const urlArray = url.split('/')
+    const author = document.querySelector('.timeline-comment-header-text .author')
+
+
+    const org = urlArray[3],
+        repo = urlArray[4],
+        PRNumber = urlArray[6],
+        PRauthor = author.text;
+
+    fetch(`https://0b27f0c0.ngrok.io/prbounty?org=${org}&repo=${repo}&prno=${PRNumber}&author=${PRauthor}`, {
+        method: 'GET',
+        headers: {
+            "Accept-Type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(res => {
+        const bountyAmountElement = document.querySelector('.current-bounty');
+        const span = document.createElement('span');
+
+        span.innerText = `Current bounty amount: ${res.amount} JECoin`
+        bountyAmountElement.appendChild(span);
+    });
+
+});
+
+
+function injectBountyInfoIntoPage() {
     const sidebarDiscussionNode = document.querySelector('div.discussion-sidebar-item.sidebar-labels.js-discussion-sidebar-item')
     const parentNode = sidebarDiscussionNode.parentNode;
 
-    console.log(sidebarDiscussionNode)
+    const bountyElement = createBountyElement();
+
+    const bla = parentNode.insertBefore(bountyElement, sidebarDiscussionNode);
+
+}
+
+function createBountyElement() {
 
     const divToInject =
         `<div>
             <button type="button" class="discussion-sidebar-heading discussion-sidebar-toggle js-menu-target">
                 Bounty
             </button>
-            <button type="submit">
+            <div class="current-bounty">
+            </div>
+            <button class="btn btn-sm" type="submit">
                 Add JECoin to PR bounty
             </button>
         </div>`
@@ -23,9 +63,5 @@ window.addEventListener('DOMContentLoaded', event => {
 
     ourDiv.innerHTML = divToInject;
 
-    const bla = parentNode.insertBefore(ourDiv, sidebarDiscussionNode);
-
-
-
-
-})
+    return ourDiv;
+}
